7eeb789211db3bbbd86b2b5a97f000b57baa46d3
Purpl3Dev <96989690+Purpl3Dev@users.noreply.github.com>
2022-01-05 09:48:28 +0000

    Update EmailCracker.sh

--- EmailCracker.sh
+++ EmailCracker.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo -e "\e[33mPrerequisites: hydra-gtk \e[0m"
-echo -e "Writed By:  \e[95mPurpl3Dev \e[0m"
+echo -e "Auth:  \e[95mPurpl3Dev \e[0m"
 echo -e "\e[91m NOTE: Make sure you have wordlists! \e[0m"
 echo -e "If you dont have one check our recommended: \e[38;5;82mhttps://bit.ly/3JyARYc]"
 echo -e "\e[97;101mDo not use this script with malicious intentions, remember that nothing on the web is completely untraceable. \e[0m"
